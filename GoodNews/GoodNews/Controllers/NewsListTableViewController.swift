46df43a4515a64a754ce5effd08edb84a294985a
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-05 20:48:23 +0000

    add rxswift pod and start using exemples of rxswift on the project for make url call

--- GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -7,16 +7,20 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsListTableViewController: UITableViewController {
     
     private var articleListViewModel: ArticleListViewModel?
+    private lazy var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        populateNews()
     }
     
+    //normal call without rxSwift just replace on viewDidLoad the call
     private func setup() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=cebc5a0f8fa34a869086e5b3e37f3db5") else { return }
@@ -30,6 +34,27 @@ class NewsListTableViewController: UITableViewController {
             
         }
     }
+    // call with rxSwift 
+    private func populateNews() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=cebc5a0f8fa34a869086e5b3e37f3db5") else { return }
+        
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> [Article]? in
+                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+            }.subscribe(onNext: { [weak self] articles in
+                if let articles = articles {
+                    DispatchQueue.main.async {
+                        self?.articleListViewModel = ArticleListViewModel(articles: articles)
+                        self?.tableView.reloadData()
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         guard let articleListViewModel = articleListViewModel else {
