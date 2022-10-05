6bd68d8f9fd5531c0ff6f5184adc1344ced592d1
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-05 20:58:43 +0000

    refactor code for api call

--- GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -34,33 +34,28 @@ class NewsListTableViewController: UITableViewController {
             
         }
     }
-    // call with rxSwift 
+    // call with rxSwift
     private func populateNews() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=cebc5a0f8fa34a869086e5b3e37f3db5") else { return }
         
-        Observable.just(url)
-            .flatMap { url -> Observable<Data> in
-                let request = URLRequest(url: url)
-                return URLSession.shared.rx.data(request: request)
-            }.map { data -> [Article]? in
-                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
-            }.subscribe(onNext: { [weak self] articles in
-                if let articles = articles {
-                    DispatchQueue.main.async {
-                        self?.articleListViewModel = ArticleListViewModel(articles: articles)
-                        self?.tableView.reloadData()
-                    }
+        let resource = Resource<ArticleList>(url: url)
+        URLRequest.load(resource: resource).subscribe(onNext: { [weak self] articles in
+            if let articles = articles {
+                DispatchQueue.main.async {
+                    self?.articleListViewModel = ArticleListViewModel(articles: articles.articles)
+                    self?.tableView.reloadData()
                 }
-            }).disposed(by: disposeBag)
+            }
+        }).disposed(by: disposeBag)
     }
-
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         guard let articleListViewModel = articleListViewModel else {
             return 0
         }
-
+        
         return articleListViewModel.numberOfSection
     }
     
@@ -70,7 +65,7 @@ class NewsListTableViewController: UITableViewController {
         }
         
         return articleListViewModel.numberOfRowsInSection(section)
-
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
